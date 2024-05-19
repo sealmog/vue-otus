06ff9fa33efd7116eefb3005453d976476b2822b
sealmo <sealmo@gmail.com>
2024-05-20 00:41:16 +0500

    Task 2. Main-page and v-for

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,53 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+
+import MainPage from '@/components/MainPage.vue'
+// import DelayFunction from "./components/DelayFunction.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <Suspense>
+    <main>
+      <main-page></main-page>
+    </main>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <!-- <delay-function></delay-function> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
+  color: #2c3e50;
+  max-width: 1280px;
+  margin: 80px auto;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+#nav {
+  padding: 16px 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(37, 37, 37);
+  color: white;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+  a {
+    color: white;
+    text-decoration: none;
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    &.router-link-exact-active {
+      color: #00b956;
+    }
   }
 }
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,98 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue'
+
+const products = ref([])
+
+const { data } = await axios.get('https://fakestoreapi.com/products')
+products.value = data
+
+// fetch('https://fakestoreapi.com/products')
+// .then((response) => response.json())
+// .then((data) => (products.value = data))
+</script>
+
+<template>
+  <div class="home">
+    <div class="products">
+      <div v-for="(product, index) in products" :key="index" class="product">
+        <div class="product-image" :style="{ backgroundImage: 'url(' + product.image + ')' }"></div>
+        <h4>{{ product.title }}</h4>
+        <p class="price">{{ product.price.toFixed(2) }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.home {
+  .products {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .product {
+      flex: 0 0 30%;
+      box-sizing: border-box;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      padding: 16px;
+      margin: 8px;
+      height: 360px;
+
+      @media only screen and (max-width: 769px) {
+        flex: 0 0 40%;
+      }
+
+      @media only screen and (max-width: 640px) {
+        flex: 0 0 90%;
+      }
+
+      &.inBag {
+        border: 1px solid #00b956;
+      }
+
+      .product-image {
+        margin: 20px auto;
+        width: 160px;
+        height: 140px;
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+      }
+      h4 {
+        margin: 22px auto;
+        font-size: 12px;
+        max-width: 60%;
+        font-weight: normal;
+      }
+
+      p.price {
+        font-size: 20px;
+        font-weight: bold;
+      }
+
+      button {
+        color: #fff;
+        background-color: #00b956;
+        border: 1px solid #00b956;
+        border-radius: 100px;
+        font-weight: 400;
+        text-align: center;
+        padding: 8px 16px;
+        cursor: pointer;
+
+        &:hover {
+          opacity: 0.8;
+        }
+
+        &.remove {
+          background-color: transparent;
+          border: none;
+          color: black;
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+}
+</style>
